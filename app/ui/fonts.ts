bc14c4e6111d84173cfc476343679bbae95f4902
Eugene Otieno Karewa <eugenekarewa223@gmail.com>
2024-05-28 08:59:05 +0300

    updated app/ui/fonts.tx

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,16 @@
-// app/ui/fonts.ts
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
-export const inter = {
-    className: 'your-font-class',
-    // other font details if any
-  };
-  
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter?.className ?? ''} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
